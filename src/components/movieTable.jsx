2c6f38bf48e749d1d2c0999016e94f6b23cc5a66
Sergio Nieto <snieto10@gmail.com>
2022-09-26 13:50:47 -0400

    step 22

--- src/components/movieTable.jsx
+++ src/components/movieTable.jsx
@@ -29,7 +29,7 @@ class MovieTable extends Component {
   ];
 
   render() {
-    const { movies, onDelete, onLike, onSort, sortColumn } = this.props;
+    const { movies, onSort, sortColumn } = this.props;
     return (
       <table className="table">
         <TableHeader
