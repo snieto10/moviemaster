2eb9d08399d1bbbe1a6c97405d065d07993fb175
Sergio Nieto <snieto10@gmail.com>
2022-09-28 16:35:25 -0400

    form step 6

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  username = React.createRef();
-
-  componentDidMount = () => {
-    this.username.current.focus();
+  state = {
+    account: { username: "", password: "" },
   };
 
   handleSubmit = (e) => {
@@ -15,7 +13,15 @@ class LoginForm extends Component {
     console.log("Submitted");
   };
 
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
+
     return (
       <div>
         <h1>Login</h1>
@@ -23,15 +29,24 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              ref={this.username}
+              value={account.username}
+              onChange={this.handleChange}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
