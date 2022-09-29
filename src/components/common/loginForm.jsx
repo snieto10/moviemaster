c4eed4eb4f812fa6eb589426a9b1777e0a9a7141
Sergio Nieto <snieto10@gmail.com>
2022-09-29 17:49:58 -0400

    step 13

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -1,5 +1,5 @@
-import { editableInputTypes } from "@testing-library/user-event/dist/utils";
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./input";
 
 class LoginForm extends Component {
@@ -8,7 +8,17 @@ class LoginForm extends Component {
     errors: {},
   };
 
+  schema = {
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  };
+
   validate = () => {
+    const result = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false,
+    });
+    console.log(result);
+
     const errors = {};
 
     const { account } = this.state;
